7dd63884f7e817dfc70649677e5620eeb96a29cb
oM <119602009+omcodedthis@users.noreply.github.com>
2023-11-06 00:27:59 +0800

    Added ability to select link + output data to .txt file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
 import java.io.IOException;
+import java.util.Scanner;
 
+
+/** The Main class gets the requested URL from the user & passes it to the TextGatherer Class. */
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        TextGatherer tg = new TextGatherer("https://en.wikipedia.org/wiki/Scrape");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the URL: ");
+        String url = scan.nextLine();
+
+        TextGatherer tg = new TextGatherer(url);
         String text = tg.gatherText();
         tg.parseText(text);
     }
--- src/main/java/TextGatherer.java
+++ src/main/java/TextGatherer.java
@@ -3,20 +3,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 
 
 /** This class handles the gathering & parsing of text from a given URL. */
 public class TextGatherer {
     private String url;
+    private PrintWriter textFile;
 
-    /** Constructor for this class, setting the instance variable. */
-    public TextGatherer(String u) {
+    /** Constructor for this class, setting the instance variable & creates the .txt file where all
+     * the data will be printed into. */
+    public TextGatherer(String u) throws FileNotFoundException {
         url = u;
+        textFile = new PrintWriter("PageData.txt");
     }
 
 
@@ -34,26 +34,46 @@ public class TextGatherer {
         Elements ids = document.select("[id]");
         Elements titles = document.select("[title]");
         Elements links = document.select("a[href^=\"https\"]");
+        Elements ptexts = document.select("p");
+        Elements atexts = document.select("a");
+
 
         // System.out.println(response); to view the response string for debugging purposes.
-        printAllEntities(ids, "id");
         printAllEntities(titles, "title");
         printAllEntities(links, "href");
+        printText(ptexts, "p");
+        printText(atexts, "a");
+        System.out.println("A PageData.txt file has been created in your current working directory which contains the webpage data.");
     }
 
 
     /** Loops over all entities in the Elements variable & prints them to the terminal. */
     private void printAllEntities(Elements group, String attribute) {
-        System.out.println("List of all the " + attribute + "s from this webpage:");
+        textFile.println("+--------------------------------------------------------------------------------------------------+");
+        textFile.println("List of all the " + attribute + "s from this webpage:");
+        textFile.println("+--------------------------------------------------------------------------------------------------+");
 
         for (Element member : group) {
             String text = member.attr(attribute); // .text() returns the text within the tag.
 
-            System.out.println(text);
+            textFile.println(text);
         }
-        System.out.println("---------------------------------------------------------------------");
     }
 
+
+    /** Loops over all entities in the Elements variable & prints its text to the terminal. */
+    private void printText(Elements group, String attribute) {
+        textFile.println("+--------------------------------------------------------------------------------------------------+");
+        textFile.println("List of all the " + attribute + "s from this webpage:");
+        textFile.println("+--------------------------------------------------------------------------------------------------+");
+
+        for (Element member : group) {
+            String text = member.text(); // .text() returns the text within the tag.
+
+            textFile.println(text);
+        }
+
+    }
 
 
     /** Gets the output (a HTML doc) from by creating a HttpURLConnection & returning it as a
